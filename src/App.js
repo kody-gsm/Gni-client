410e8eee8fd48d89edc4f346c20d87e6bb9bb30a
kkh061101 <kkh061101@naver.com>
2023-06-08 18:00:49 +0900

    진짜 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+
 const App = () => {
   return (
     <div className="app">
-      ㅘㅏㅏㅏㅏㅏ
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
