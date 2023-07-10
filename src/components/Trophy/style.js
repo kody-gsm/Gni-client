2bf72f05b8029d36f0f5708dbc987df4c25b1caa
kkh061101 <kkh061101@naver.com>
2023-07-10 22:36:06 +0900

    Trophy 안 글씨크기 더 작게

--- src/components/Trophy/style.js
+++ src/components/Trophy/style.js
@@ -19,5 +19,7 @@ export const Trophy = styled.div`
     margin-top: -60px;
     color: white;
     font-size: 10px;
+    -webkit-transform: scale(0.8); //0.5 -> 50%
+    display: inline-block;
   }
 `
