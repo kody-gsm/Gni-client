7da4e0dea310e8b23a1c32d5dfa301147d983ba1
윤수용 <yunsuyong739@naver.com>
2023-08-23 22:52:57 +0900

    커뮤니티 페이지에서 글 작성 모달 띄울수 있도록 수정

--- src/pages/Community/index.jsx
+++ src/pages/Community/index.jsx
@@ -1,53 +1,126 @@
 import Nav from "../../components/Nav/Nav";
-import React, { useState, useEffect } from 'react';
-import * as S from './style';
-import bell from '../../assets/bell.png';
+import React, { useState, useEffect } from "react";
+import * as S from "./style";
+import bell from "../../assets/bell.png";
 import Boxcontent from "../../components/Boxcontent/boxcontent";
+import { createPortal } from "react-dom";
+import WritePost from "../../components/WritePost";
 
 export default function Community() {
   const [belling, setBelling] = useState(true);
+  const [createModal, setCreateModal] = useState(false);
   const [index, setIndex] = useState(0);
-  useEffect(e => {
-    setTimeout(() => {
-      setBelling(false);
-    }, 3000);
-  }, [belling]);
+  useEffect(
+    (e) => {
+      setTimeout(() => {
+        setBelling(false);
+      }, 3000);
+    },
+    [belling]
+  );
   function MakeDot(cnt) {
     let t = [];
     for (let i = 0; i < cnt; i++) {
-      t.push(<div className={`dot ${index === i && `active`}`} onClick={e => setIndex(i)} />);
+      t.push(
+        <div
+          className={`dot ${index === i && `active`}`}
+          onClick={(e) => setIndex(i)}
+        />
+      );
     }
     return t;
   }
-  return <>
-    <Nav />
-    <S.Community>
-      <div className="whitebox">
-        <div className="boxheader">
-          <div className="title">
-            <img src={bell} alt="bell" className={belling ? 'active' : ''} onClick={e => setBelling(true)} />
-            <span>지금 커뮤니티 인기 순위 게시글들이에요!</span>
+  return (
+    <>
+      <Nav />
+      <S.Community>
+        <div className="whitebox">
+          <div className="boxheader">
+            <div className="title">
+              <img
+                src={bell}
+                alt="bell"
+                className={belling ? "active" : ""}
+                onClick={(e) => setBelling(true)}
+              />
+              <span>지금 커뮤니티 인기 순위 게시글들이에요!</span>
+            </div>
+            <button
+              className="gowrite"
+              onClick={(e) => (setCreateModal(true))}
+            >
+              커뮤니티 글쓰러 가기
+            </button>
           </div>
-          <button className="gowrite" onClick={e => window.location.href = '/write'}>
-            커뮤니티 글쓰러 가기
-          </button>
+          <div className="main">
+            {
+              <>
+                <Boxcontent
+                  name={"홍길동"}
+                  title={"대충 아무 텍스트"}
+                  answers={999}
+                  likes={999}
+                  checking={false}
+                  trophy={1}
+                />
+                <Boxcontent
+                  name={"홍길동"}
+                  title={"대충 아무 텍스트"}
+                  answers={999}
+                  likes={999}
+                  checking={false}
+                  trophy={2}
+                />
+                <Boxcontent
+                  name={"홍길동"}
+                  title={"대충 아무 텍스트"}
+                  answers={999}
+                  likes={999}
+                  checking={false}
+                  trophy={3}
+                />
+                <Boxcontent
+                  name={"홍길동"}
+                  title={"대충 아무 텍스트"}
+                  answers={999}
+                  likes={999}
+                  checking={false}
+                />
+                <Boxcontent
+                  name={"홍길동"}
+                  title={"대충 아무 텍스트"}
+                  answers={999}
+                  likes={999}
+                  checking={false}
+                />
+                <Boxcontent
+                  name={"홍길동"}
+                  title={"대충 아무 텍스트"}
+                  answers={999}
+                  likes={999}
+                  checking={false}
+                />
+                <Boxcontent
+                  name={"홍길동"}
+                  title={"대충 아무 텍스트"}
+                  answers={999}
+                  likes={999}
+                  checking={false}
+                />
+                <Boxcontent
+                  name={"홍길동"}
+                  title={"대충 아무 텍스트"}
+                  answers={999}
+                  likes={999}
+                  checking={false}
+                />
+              </>
+            }
+          </div>
+          <div className="dots">{MakeDot(4)}</div>
         </div>
-        <div className="main">
-          {<>
-            <Boxcontent name={'홍길동'} title={'대충 아무 텍스트'} answers={999} likes={999} checking={false} trophy={1} />
-            <Boxcontent name={'홍길동'} title={'대충 아무 텍스트'} answers={999} likes={999} checking={false} trophy={2} />
-            <Boxcontent name={'홍길동'} title={'대충 아무 텍스트'} answers={999} likes={999} checking={false} trophy={3} />
-            <Boxcontent name={'홍길동'} title={'대충 아무 텍스트'} answers={999} likes={999} checking={false} />
-            <Boxcontent name={'홍길동'} title={'대충 아무 텍스트'} answers={999} likes={999} checking={false} />
-            <Boxcontent name={'홍길동'} title={'대충 아무 텍스트'} answers={999} likes={999} checking={false} />
-            <Boxcontent name={'홍길동'} title={'대충 아무 텍스트'} answers={999} likes={999} checking={false} />
-            <Boxcontent name={'홍길동'} title={'대충 아무 텍스트'} answers={999} likes={999} checking={false} />
-          </>}
-        </div>
-        <div className="dots">
-          {MakeDot(4)}
-        </div>
-      </div>
-    </S.Community>
-  </>;
+      </S.Community>
+      {createModal && createPortal(<WritePost setModal={setCreateModal}/> , document.body)}
+    </>
+  );
 }
